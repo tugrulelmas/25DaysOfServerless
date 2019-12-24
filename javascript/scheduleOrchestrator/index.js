7ae0132602879e88bb322729cd7e7eda1ab26679
Tugrul Elmas <tugrulelmas@gmail.com>
2019-12-24 14:35:42 +0300

    added durable function

---
+++ javascript/scheduleOrchestrator/index.js
@@ -0,0 +1,19 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const input = context.df.getInput();
+    yield context.df.createTimer(new Date(input.startAt));
+
+    return yield context.df.callActivity('sendEmail', input);
+});
---
+++ javascript/sendEmail/index.js
@@ -0,0 +1,26 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+const mailgun = require('mailgun.js');
+
+module.exports = async function (context) {
+    const mg = mailgun.client({username: 'api', key: process.env["MailgunKey"]});
+    const { email, title, startAt, description } = context.bindings.payload;
+    const mailMessage = {
+        from: process.env["FromEmail"],
+        to: [email],
+        subject: `Test: ${title}`,
+        text: "Testing some Mailgun awesomness!",
+        html: `<h4>${title} @ ${startAt}</h4> <p>${description}</p>`
+      };
+
+    mg.messages.create(process.env["FromDomain"], mailMessage);
+    return msg;
+};
